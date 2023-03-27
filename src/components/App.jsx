e8bd9b87a0e8fb103703fe42b2b931db322e6f74
Яричук Альона <alenamasha@gmail.com>
2023-03-27 09:41:19 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Message } from './Message/Message';
+import css from '../components/App.module.css';
+
+const LOCAL_KEY = 'contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(LOCAL_KEY);
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem(LOCAL_KEY, JSON.stringify(nextContacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const newContact = { id: nanoid(), name, number };
+
+    contacts.some(contacts => contacts.name === name)
+      ? alert(`${name}, This user is already in the contact list.`)
+      : this.setState(({ contacts }) => ({
+          contacts: [newContact, ...contacts],
+        }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  filterContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizFilter)
+    );
+  };
+
+  render() {
+    const filterContacts = this.filterContacts();
+    const length = this.state.contacts.length;
+    return (
+      <div className={css.styleApp}>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} changeFilter={this.changeFilter} />
+        {length > 0 ? (
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : (
+          <Message text="Contact list is empy." />
+        )}
+      </div>
+    );
+  }
+}
